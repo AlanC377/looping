0ae0056d939a686309fadadf1cd9bc2fe77939df
Alan <alanc37777@students.laalliance.org>
2017-11-17 17:53:38 +0000

    Watermelon.

---
+++ loops.js
@@ -0,0 +1,12 @@
+var age = 1
+
+while (age < 20) {
+    console.log('I am ' + age + ' years old.')
+    console.log("I'm still a teenager")
+    age = age + 1
+}
+
+console.log("Now I am " + age + " years old.")
+
+
+
